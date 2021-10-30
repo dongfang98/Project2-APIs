bdf89e7dd6fb907503e148393b96b5160835d7c0
dongfang98 <78338843+dongfang98@users.noreply.github.com>
2021-10-29 23:13:08 -0400

    Update TWeetAPI.py

--- action test/TWeetAPI.py
+++ action test/TWeetAPI.py
@@ -8,12 +8,14 @@ access_token = os.getenv('ACCESS_TOKEN')
 access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
 consumer_key = os.getenv('CONSUMER_KEY')
 consumer_secret = os.getenv('CONSUMER_SECRET')
-
 def Authorization_Setup():
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #pass customer keys
-    auth.set_access_token(access_token, access_token_secret) #pass access_token and access_token_secret
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-    return api #generate the api
+    try:
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        return api
+    except Exception:
+        sys.exit("Reach limit, cannot connect to API")
 
 
 # Print out tweets' text.!!!

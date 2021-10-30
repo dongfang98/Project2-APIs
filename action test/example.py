b791dccb3964ab661abf3f27231386c94cf75e74
dongfang98 <78338843+dongfang98@users.noreply.github.com>
2021-10-29 22:14:48 -0400

    Add files via upload

---
+++ action test/example.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def add(a,b):
+  return a + b
+  
+def subtract(a,b):
+  return a - b
+
+def numpy_add(a,b):
+  return np.add(a,b)
+
+def numpy_subtract(a,b):
+  return np.subtract(a,b)
+
+#def test_add():
+#  assert add(2,3) == 5
+#  assert add('boston','university') == 'bostonuniversity'
+#  assert numpy_add(2,3) == 5
+  
+#def test_subtract():
+#  assert subtract(2,3) == -1
+#  assert numpy_subtract(2,3) == -1
